52aa92f9efafcbdb9230dbd10c1a18155a3c91f9
nginpay <p.tozzi@nginpay.com>
2021-06-15 21:47:03 -0300

    commit aula 2

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+const copyright = 'Blog do Paulo - 2021';
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Curso Node' });
+router.get('/', function(req, res, next) { //meudominio.com
+  res.render('index', 
+  { 
+    title: 'Blog do Paulo', 
+    copyright 
+  });
 });
 
-router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Página de contatos'})
+router.get('/about', function (req, res, next) {
+  res.render('about', {title: 'Sobre o autor', copyright })
+})
+
+router.get('/posts', function (req, res, next) {
+  res.render('posts', {title: 'Meus posts', copyright})
+})
+
+router.get('/categories', function (req, res, next) {
+  res.render('categories', {title: 'Categorias', copyright})
+})
+
+router.get('/contact', function(req, res, next) { //meudominio.com/contact
+  res.render('contact', { title: 'Contato', copyright})
 })
 
 module.exports = router;
